29c950436eaee90113f7162adb102bfdc90e9a28
Yukeshan <yjyukesh@gmail.com>
2024-11-19 17:44:41 +0530

    complete login and signup forms without styling

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+     <Signup/>
+     <Login/>
+    </div>
+  );
+}
+
+
+export default App;
+
+//21:00
